e152945537b5dacc03fdb2f6aa29fb6c82b5095a
seohw4 <jerry8342@gmail.com>
2022-08-05 16:34:22 +0900

    Arrays

--- nyang.js
+++ nyang.js
@@ -1,3 +1,12 @@
-const amIPig = null;
-let something;
-console.log(something, amIPig);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+console.log(daysOfWeek);
+
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+const toBuy = ["milk", "jelly", "egg", "vinyl"];
+toBuy.push("pasta");
+console.log(toBuy);
+console.log(toBuy[2]);
