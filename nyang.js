52d28ef39416ca8b4db505b9d325fc87db113281
seohw4 <jerry8342@gmail.com>
2022-08-05 15:32:23 +0900

    const and let

--- nyang.js
+++ nyang.js
@@ -1,9 +1,13 @@
 const a = 5;
 const b = 2;
-const me = "yanggang";
+let me = "yanggang";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log(a - b);
 console.log("hello " + me);
+
+me = "nyanggang";
+
+console.log("My name is " + me);
